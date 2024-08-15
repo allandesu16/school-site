f3fe56a6dbd79984f45d46b13981a9d49c597fee
allandesu <irkzpatrick@gmail.com>
2024-08-15 20:44:56 +0200

    overview grid

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -17,7 +17,45 @@ const Overview = () => {
                     </div>
                 </div>
                 {/* grid of overview */}
-                <div className="flex flex-col bg-red-500 w-full">hello</div>
+                <div className="flex bg-red-500 w-full h-full gap-4">
+                    {/* first grid */}
+                    <div className="flex w-full bg-purple-500 gap-4 p-2">
+                        <div className="flex bg-orange-500 w-full">
+                            grid subgrid
+                        </div>
+                        <div className="flex gap-2 flex-col bg-orange-500 w-full">
+                            <div className="flex bg-green-500 h-full w-full">
+                                yup
+                            </div>
+                            <div className="flex bg-green-500 h-full w-full">
+                                yup
+                            </div>
+                        </div>
+                    </div>
+                    {/* second grid */}
+                    <div className="grid w-full gap-4">
+                        <div className="flex gap-2 bg-blue-500 p-2">
+                            <div className="flex bg-orange-500 p-2 w-full">
+                                grid subgrid 2
+                            </div>
+                            <div className="flex bg-orange-500 p-2 w-full">
+                                grid subgrid 2
+                            </div>
+                        </div>
+                        <div className="flex bg-blue-500 gap-2 p-2">
+                            <div className="flex bg-orange-500 p-2 w-full">
+                                grid subgrid 2
+                            </div>
+
+                            <div className="flex bg-orange-500 p-2 w-full">
+                                grid subgrid 2
+                            </div>
+                            <div className="flex bg-orange-500 p-2 w-full">
+                                grid subgrid 2
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
