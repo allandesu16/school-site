911583cfee6b5ca3bf0ce3e02c40e547b0599c6a
allandesu <irkzpatrick@gmail.com>
2024-08-15 19:51:22 +0200

    overview slideshow

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -4,11 +4,20 @@ import React from "react";
 
 const Overview = () => {
     return (
-        <div className="flex max-container p-6 rounded-xl w-full h-full">
+        <div className="flex rounded-xl w-full h-full p-6">
             <div className="flex w-full flex-col gap-6">
                 {/* nutrition,hostels,sports,religion,discipline,entertainment */}
                 {/* slideshow */}
-                <div className="flex w-full h-[60%] object-contain "></div>
+                <div className="flex w-full gap-6 h-[60vh] bg-slate-500">
+                    <div className="flex h-[90%] w-[45vw]  mt-0 bg-green-700">
+                        hello
+                    </div>
+                    <div className="flex h-[90%] w-[45vw]  mt-0 bg-green-700">
+                        hello
+                    </div>
+                </div>
+                {/* grid of overview */}
+                <div className="flex flex-col bg-red-500 w-full">hello</div>
             </div>
         </div>
     );
